9f42bcf05e4339354f69c0387fb816e78602a507
Kod Enigma <olamilekanadeleke123@gmail.com>
2024-01-08 18:52:22 +0100

    update

--- JobSpot/Features/Auth/Views/Login/Components/LoginButtonsView.swift
+++ JobSpot/Features/Auth/Views/Login/Components/LoginButtonsView.swift
@@ -10,8 +10,9 @@ import UIKit
 final class LoginButtonsView: UIView {
     private let loginButton = BaseButton(viewModel: .init(type: .primary, title: "Login"))
     private let googleLoginButton = BaseButton(viewModel: .init(type: .secondary, title: "Login With Google"))
+    private let createAccountLabel = Label(type: .thin)
 
-    private let vStack = stackView(spacing: 15)
+    private let vStack = stackView(spacing: 10)
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -30,12 +31,27 @@ extension LoginButtonsView {
     public func style() {
         translatesAutoresizingMaskIntoConstraints = false
 
-//        loginButton.b
+        createAccountLabel.textAlignment = .center
+        createAccountLabel.font = .systemFont(ofSize: 12, weight: .thin)
+
+        let questionString = NSMutableAttributedString(string: "You don't have an account yet? ", attributes: [
+            .font: UIFont.systemFont(ofSize: 12, weight: .thin),
+        ])
+        let signUpString = NSMutableAttributedString(string: "Sign Up", attributes: [
+            .font: UIFont.systemFont(ofSize: 12, weight: .thin),
+            .foregroundColor: UIColor(red: 0.99, green: 0.64, blue: 0.3, alpha: 1),
+            .underlineColor: UIColor(red: 0.99, green: 0.64, blue: 0.3, alpha: 1),
+            .underlineStyle: NSUnderlineStyle.single.rawValue,
+        ])
+
+        questionString.append(signUpString)
+        createAccountLabel.attributedText = questionString
     }
 
     public func layout() {
         vStack.addArrangedSubview(loginButton)
         vStack.addArrangedSubview(googleLoginButton)
+        vStack.addArrangedSubview(createAccountLabel)
 
         addSubview(vStack)
     }
