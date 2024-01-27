1187b24b53451f5e9019c70c7338b37540a7aa33
Kod Enigma <olamilekanadeleke123@gmail.com>
2024-01-27 22:26:28 +0100

    feat: added company image to segmented view on company details view

--- JobSpot/Features/Company/Views/Components/CampanyDetailsSegmentedView/CompanyDetailAboutUsView.swift
+++ JobSpot/Features/Company/Views/Components/CampanyDetailsSegmentedView/CompanyDetailAboutUsView.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class CompanyDetailAboutUsView: UIView {
-    private let vStack = stackView()
+    private let vStack = stackView(spacing: 15)
     private let aboutText = TextOptionView()
     private let websiteText = TextOptionView()
     private let industryText = TextOptionView()
@@ -17,6 +17,9 @@ class CompanyDetailAboutUsView: UIView {
     private let typeText = TextOptionView()
     private let sinceText = TextOptionView()
     private let specializationText = TextOptionView()
+    private let companyGallery = TextOptionView()
+
+    private let companyImageView = CompanyImageView()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -45,31 +48,24 @@ extension CompanyDetailAboutUsView {
         typeText.configure(title: "Type", subtitle: "Multinational company")
         sinceText.configure(title: "Since", subtitle: "1998")
         specializationText.configure(title: "Specialization", subtitle: "Search technology, Web computing, Software and Online advertising")
+        companyGallery.configure(title: "Comapny Gallery", subtitle: "")
+
+        companyImageView.configure(images: ["company_image_one", "company_image_two", "company_image_two", "company_image_two"])
     }
 
     private func layout() {
+        vStack.addArrangedSubview(BoxView())
         vStack.addArrangedSubview(aboutText)
-
-        vStack.setCustomSpacing(15, after: aboutText)
         vStack.addArrangedSubview(websiteText)
-
-        vStack.setCustomSpacing(15, after: websiteText)
         vStack.addArrangedSubview(industryText)
-
-        vStack.setCustomSpacing(15, after: industryText)
         vStack.addArrangedSubview(employSizeText)
-
-        vStack.setCustomSpacing(15, after: employSizeText)
         vStack.addArrangedSubview(headOfficeText)
-
-        vStack.setCustomSpacing(15, after: headOfficeText)
         vStack.addArrangedSubview(typeText)
-
-        vStack.setCustomSpacing(15, after: typeText)
         vStack.addArrangedSubview(sinceText)
-
-        vStack.setCustomSpacing(15, after: sinceText)
         vStack.addArrangedSubview(specializationText)
+        vStack.addArrangedSubview(companyGallery)
+
+        vStack.addArrangedSubview(companyImageView)
 
         addSubview(vStack)
         vStack.pinToEdges(to: self)
@@ -117,5 +113,86 @@ private class TextOptionView: UIView {
 
         addSubview(vStack)
         vStack.pinToEdges(to: self)
+    }
+}
+
+// MARK: - Company Image
+
+private class CompanyImageView: UIView {
+    private let hStack = stackView(axis: .horizontal)
+    private var images = [String]()
+
+    private let boxView = BoxView()
+    private let moreText = Label(type: .regular)
+
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    open func configure(images: [String]) {
+        self.images = images
+
+        layout()
+        style()
+    }
+
+    private func style() {
+        translatesAutoresizingMaskIntoConstraints = false
+
+        boxView.frame = CGRect(x: 0, y: 0, width: Config.width, height: Config.height)
+        boxView.alpha = 0.7
+        boxView.backgroundColor = UIColor(red: 0.048, green: 0.21, blue: 0.283, alpha: 1)
+        boxView.layer.cornerRadius = 8
+
+        moreText.text = "+\(images.count) pictures"
+        moreText.font = .systemFont(ofSize: 12, weight: .bold)
+        moreText.textColor = UIColor.white
+    }
+
+    private func layout() {
+        hStack.distribution = .equalSpacing
+
+        let imageOne = ImageView(image: UIImage(named: images[0]))
+        let imageTwo = ImageView(image: UIImage(named: images[1]))
+
+        imageOne.contentMode = .scaleAspectFit
+        imageTwo.contentMode = .scaleAspectFit
+
+        imageOne.layer.cornerRadius = 8
+        imageOne.clipsToBounds = true
+        imageTwo.layer.cornerRadius = 8
+        imageTwo.clipsToBounds = true
+
+        hStack.addArrangedSubview(imageOne)
+        hStack.addArrangedSubview(imageTwo)
+        NSLayoutConstraint.activate([
+            imageOne.heightAnchor.constraint(equalToConstant: Config.height),
+            imageOne.widthAnchor.constraint(equalToConstant: Config.width),
+
+            imageTwo.heightAnchor.constraint(equalToConstant: Config.height),
+            imageTwo.widthAnchor.constraint(equalToConstant: Config.width),
+        ])
+
+        imageTwo.addSubview(boxView)
+        boxView.pinToEdges(to: imageTwo)
+
+        imageTwo.addSubview(moreText)
+        NSLayoutConstraint.activate([
+            moreText.centerXAnchor.constraint(equalTo: imageTwo.centerXAnchor),
+            moreText.centerYAnchor.constraint(equalTo: imageTwo.centerYAnchor),
+        ])
+
+        addSubview(hStack)
+        hStack.pinToEdges(to: self)
+    }
+
+    enum Config {
+        static let height = 100.h()
+        static let width = 44.sw()
     }
 }
